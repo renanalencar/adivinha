1a6438a255d162eeb1299a9eb1cc403c2f14b28d
Renan Costa Alencar <renan.costaalencar@gmail.com>
2019-07-05 11:18:35 -0300

    #App1 - Como usar TextViews (Atualizado)

--- app/src/main/java/com/cursoandroid/adivinha/MainActivity.java
+++ app/src/main/java/com/cursoandroid/adivinha/MainActivity.java
@@ -6,34 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.botaoJogarId);
+        textoResultado = findViewById(R.id.textoResultadoId);
+
+        textoResultado.setText("Texto alterado");
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
