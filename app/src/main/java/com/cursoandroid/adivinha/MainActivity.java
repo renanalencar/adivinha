3454a2c5b0fe82d492c54af60f8d88d84e9d8e1f
Renan Costa Alencar <renan.costaalencar@gmail.com>
2019-07-05 11:28:42 -0300

    #App1 - Como usar Buttons

--- app/src/main/java/com/cursoandroid/adivinha/MainActivity.java
+++ app/src/main/java/com/cursoandroid/adivinha/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +25,18 @@ public class MainActivity extends AppCompatActivity {
         botaoJogar = findViewById(R.id.botaoJogarId);
         textoResultado = findViewById(R.id.textoResultadoId);
 
-        textoResultado.setText("Texto alterado");
+        //textoResultado.setText("Texto alterado");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+                textoResultado.setText("Número escolhido: " + numeroAleatorio);
+
+            }
+        });
 
     }
 
